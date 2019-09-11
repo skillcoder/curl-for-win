fc98cab81230b09a91cc0cc45ab67001c8eb90d7
Viktor Szakats <commit@vszakats.net>
2019-09-11 10:59:30 +0000

    openssl.sh: update for OpenSSL 1.1.1d and upper
    This patch drops the local override of a secure OpenSSL
    prefix (`C:/Windows/System32/OpenSSL`) for the Windows
    platform and now uses the fixed and now secure default
    of OpenSSL (`C:/Program Files/OpenSSL' and
    `C:/Program Files/Common Files/SSL`). This also made it
    possible to drop the local patch that let ./Configure
    allow a custom Windows prefix be passed.
    
    Also adapted packaging logic to the new layout of
    DESTDIR and added ct_log_list.cnf and *.dist copies to
    the distribution package.
    
    Finally add an OpenSSL 1.1.1d patch to fix `make
    install` with prefixes with spaces in them (such as the
    new default prefix when building for Windows) to work
    on *nix platforms with cross-builds.
    
    The patch was submitted upstream here:
    https://github.com/openssl/openssl/pull/9864

--- openssl.sh
+++ openssl.sh
@@ -62,35 +62,23 @@ _cpu="$2"
   # AR=, NM=, RANLIB=
   unset CC
 
-  # Patch OpenSSL ./Configure to make it accept Windows-style absolute
-  # paths as --prefix. Without the patch it misidentifies all such
-  # absolute paths as relative ones and aborts.
-  sed 's|die "Directory given with --prefix|print "Directory given with --prefix|g' \
-    < ./Configure > ./Configure-patched
-  chmod a+x ./Configure-patched
-
-  # Space or backslash not allowed. Needs to be a folder restricted
-  # to Administrators across majority of Windows installations, versions
-  # and configurations.
-  _prefix='C:/Windows/System32/OpenSSL'
-
   # shellcheck disable=SC2086
-  ./Configure-patched ${options} shared \
+  ./Configure ${options} shared \
     "--cross-compile-prefix=${_CCPREFIX}" \
     -fno-ident \
     -Wl,--nxcompat -Wl,--dynamicbase \
     no-unit-test \
     no-idea \
     no-tests \
-    no-makedepend \
-    "--prefix=${_prefix}"
+    no-makedepend
   SOURCE_DATE_EPOCH=${unixts} TZ=UTC make
   # Install it so that it can be detected by CMake
   # (ending slash required)
   make install "DESTDIR=$(pwd)/pkg/" > /dev/null # 2>&1
 
-  # DESTDIR= + --prefix=
-  _pkg="pkg/${_prefix}"
+  # DESTDIR= + default prefixes (assumes OpenSSL 1.1.1d or upper)
+  _pkg='pkg/Program Files/OpenSSL'
+  _pks='pkg/Program Files/Common Files/SSL'
 
   # Make steps for determinism
 
@@ -120,7 +108,10 @@ _cpu="$2"
     ../_sign.sh "${_ref}" ${engdir}/*.dll
   fi
 
-  touch -c -r "${_ref}" ${_pkg}/ssl/openssl.cnf
+  touch -c -r "${_ref}" ${_pks}/ct_log_list.cnf
+  touch -c -r "${_ref}" ${_pks}/ct_log_list.cnf.dist
+  touch -c -r "${_ref}" ${_pks}/openssl.cnf
+  touch -c -r "${_ref}" ${_pks}/openssl.cnf.dist
   touch -c -r "${_ref}" ${_pkg}/bin/openssl.exe
   touch -c -r "${_ref}" ${_pkg}/bin/*.dll
   touch -c -r "${_ref}" ${_pkg}/include/openssl/*.h
@@ -154,17 +145,20 @@ _cpu="$2"
     cp -f -p -r ${engdir} "${_DST}/"
   fi
 
-  cp -f -p ${_pkg}/ssl/openssl.cnf     "${_DST}/"
-  cp -f -p ${_pkg}/bin/openssl.exe     "${_DST}/"
-  cp -f -p ${_pkg}/bin/*.dll           "${_DST}/"
-  cp -f -p ${_pkg}/include/openssl/*.h "${_DST}/include/openssl/"
-  cp -f -p ${_pkg}/lib/*.a             "${_DST}/lib/"
-  cp -f -p ${_pkg}/lib/pkgconfig/*.pc  "${_DST}/lib/pkgconfig/"
-  cp -f -p CHANGES                     "${_DST}/CHANGES.txt"
-  cp -f -p LICENSE                     "${_DST}/LICENSE.txt"
-  cp -f -p README                      "${_DST}/README.txt"
-  cp -f -p FAQ                         "${_DST}/FAQ.txt"
-  cp -f -p NEWS                        "${_DST}/NEWS.txt"
+  cp -f -p ${_pks}/ct_log_list.cnf      "${_DST}/"
+  cp -f -p ${_pks}/ct_log_list.cnf.dist "${_DST}/"
+  cp -f -p ${_pks}/openssl.cnf          "${_DST}/"
+  cp -f -p ${_pks}/openssl.cnf.dist     "${_DST}/"
+  cp -f -p ${_pkg}/bin/openssl.exe      "${_DST}/"
+  cp -f -p ${_pkg}/bin/*.dll            "${_DST}/"
+  cp -f -p ${_pkg}/include/openssl/*.h  "${_DST}/include/openssl/"
+  cp -f -p ${_pkg}/lib/*.a              "${_DST}/lib/"
+  cp -f -p ${_pkg}/lib/pkgconfig/*.pc   "${_DST}/lib/pkgconfig/"
+  cp -f -p CHANGES                      "${_DST}/CHANGES.txt"
+  cp -f -p LICENSE                      "${_DST}/LICENSE.txt"
+  cp -f -p README                       "${_DST}/README.txt"
+  cp -f -p FAQ                          "${_DST}/FAQ.txt"
+  cp -f -p NEWS                         "${_DST}/NEWS.txt"
 
   # Luckily, applink is not implemented for 64-bit mingw, omit this file then
   [ "${_cpu}" = '32' ] && cp -f -p ms/applink.c "${_DST}/include/openssl/"
